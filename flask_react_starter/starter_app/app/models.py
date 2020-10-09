e318097202f4af3036233b657c79f0f563818045
djicegiggles <shaieleigh1@gmail.com>
2020-10-09 11:22:42 -0400

    database

--- flask_react_starter/starter_app/app/models.py
+++ flask_react_starter/starter_app/app/models.py
@@ -17,10 +17,10 @@ class User(db.Model):
       "email": self.email
     }
 
-class Appointment(db.Model):
-  __tablename__ = 'appointments'
+# class Appointment(db.Model):
+#   __tablename__ = 'appointments'
 
-  id = db.Column(db.Integer, primary_key = True)
-  category = db.Column(db.String(25), nullable=False)
-  date = db.Column(db.Date, nullable=False)
-  time = db.Column(db.Time, nullable=False)
+#   id = db.Column(db.Integer, primary_key = True)
+#   category = db.Column(db.String(25), nullable=False)
+#   date = db.Column(db.Date, nullable=False)
+#   time = db.Column(db.Time, nullable=False)
